70797c3b5e54cfb7d87e4872db69e532c4ac1710
ikkikana <hei.princy@gmail.com>
2025-09-03 09:52:55 +0300

    Commit

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from fastapi.openapi.models import OpenAPI
 
 app = FastAPI()
 
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
+
+@app.get("/health")
+def welcome_health():
+    return {"OK"}
+
+@app.post("/phones", status_code= 201 )
+def create_user
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
